2b34d9c3c535b14ff638b6e389865ef4f3fc7593
Hemanth Sunkireddy <hemanthkumarreddysunkireddy51@gmail.com>
2025-03-07 11:12:55 +0530

    Added Vercel Deployment configuration for backend

---
+++ backend/README.md
@@ -0,0 +1,6 @@
+# Swipe Backend
+
+* Using Express.js and deloyed in Vercel
+
+> Server Link: https://swipe-backend-hemanthsunkireddys-projects.vercel.app/
+
--- swipe-frontend/src/app/page.tsx
+++ swipe-frontend/src/app/page.tsx
@@ -1,4 +1,5 @@
-import Image from "next/image";
+// import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -18,7 +19,13 @@ export default function Home() {
           </li>
           <li>Swipe Automatic Data Extraction and Invoice Managment.</li>
         </ol>
-
+          <Link href="/invoice"> Invoice Tab
+          </Link>
+          <Link href="/file-upload"> File Upload Tab
+          </Link>
+          <Link href="https://github.com/hemanth-sunkireddy/Swipe-Invoice-Management/wiki"> Code Documentation
+          </Link>
+          
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           {/* <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
